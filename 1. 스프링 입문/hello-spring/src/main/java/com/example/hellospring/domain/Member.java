19ff0074d0edfb49989f1546a4bca5f23615c88a
seungmin <tmdals1592@naver.com>
2023-08-27 15:18:42 +0900

    수강 완료

--- 1. 스프링 입문/hello-spring/src/main/java/com/example/hellospring/domain/Member.java
+++ 1. 스프링 입문/hello-spring/src/main/java/com/example/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package com.example.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String name;
 
     public Long getId() {
--- 1. 스프링 입문/hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
+++ 1. 스프링 입문/hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -3,10 +3,14 @@ package com.example.hellospring.service;
 import com.example.hellospring.domain.Member;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
